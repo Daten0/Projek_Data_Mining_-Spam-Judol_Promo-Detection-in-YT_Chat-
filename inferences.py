4a60e77833416d9914d9791aac93424f7e0cbd57
DatenMints <febrimicrone@gmail.com>
2026-03-20 21:04:32 +0800

    Fetching chat for testing purpose

--- inferences.py
+++ inferences.py
@@ -1,8 +1,12 @@
-import pandas as pd
+import pandas as pd # not yet being used dumbass (belum kepake)
 import pytchat
+import json
 
-
-video_id = "6lC4wrcWAm4"
+# https://www.youtube.com/watch?v=ZLk2NqvCOrw
+"""
+    Marapthon live
+"""
+video_id = "ZLk2NqvCOrw"
 
 def main():
     chat = pytchat.create(video_id)
@@ -10,14 +14,22 @@ def main():
     try:
         while chat.is_alive():
             # TBA
+            for c in chat.get().sync_items():
+                # print(f"{c.datetime} [{c.author.name}]- {c.message}")
+                livechat_obj = c.json()
+                live_chat = json.loads(livechat_obj)
+                
+                print(live_chat['datetime'] + " : " + live_chat['author']['name'] + " : " + live_chat['message'])
 
-    except:
-        print("Error: Video ID tidak valid")
+    except Exception as error:
+        print(f"Error: {error}")
         # TBA
 
 if __name__ == "__main__":
     print(f"Youtube Inferences dimulai")
     print("===========Harap tunggu sampai selesai===========")
     main()
+    exit()
 
 # Belum selesai breeee
+
